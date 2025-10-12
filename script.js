83e18e7918c117a7ebd4b103ed8233d802cc056d
PRATHMESH CHAVAN <sansanmart22@gmail.com>
2025-10-12 09:43:50 +0530

    hi

--- script.js
+++
@@ -1,6 +0,0 @@
-// Add button click interactivity
-document.querySelectorAll('button').forEach(btn => {
-  btn.addEventListener('click', () => {
-    alert('Button clicked: ' + btn.textContent);
-  });
-});
