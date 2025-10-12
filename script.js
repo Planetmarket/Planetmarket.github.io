705b68e647e44b3bce0d8071c46482c37dad5973
PRATHMESH CHAVAN <sansanmart22@gmail.com>
2025-10-12 09:29:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 
+// Add button click interactivity
+document.querySelectorAll('button').forEach(btn => {
+  btn.addEventListener('click', function() {
+    alert('Button clicked: ' + this.textContent);
+  });
+});
